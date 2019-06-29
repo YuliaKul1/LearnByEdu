07fa05abc1c0a15468dd7e07d5e6e29903c45893
Yulia Kulyasova <ulechek.kul@gmail.com>
2019-06-29 20:49:12 +0400

    update SecondOptTask

--- src/main/java/fundamentals/FirstOptTask.java
+++ src/main/java/fundamentals/FirstOptTask.java
@@ -7,39 +7,42 @@ import java.util.ArrayList;
 
 public class FirstOptTask {
 
-    public static void helloUser() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            String name = reader.readLine();
-            System.out.println("Hello " + name);
-            reader.close();
+    public static void main(String[] args)  {
+        System.out.println("Hello " + helloUser());
+        System.out.println("Сумма чисел " + sumNumbers());
+    }
+
+    static String helloUser() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            return reader.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return " ";
     }
 
-    public static void sumNumbers() throws IOException {
+    static int sumNumbers() {
         ArrayList<Integer> arrayList = new ArrayList<Integer>();
         int number;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int sum = 0;
-        String inputString = reader.readLine();
-        while (!"".equals(inputString)) {
-            try {
-                number = Integer.parseInt(inputString);
-                arrayList.add(number);
-                inputString = reader.readLine();
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
-        }
-        reader.close();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String inputString = reader.readLine();
+            while (!"".equals(inputString)) {
+                try {
+                    number = Integer.parseInt(inputString);
+                    arrayList.add(number);
+                    inputString = reader.readLine();
 
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int sum = 0;
         for (Integer i : arrayList) {
             sum = sum + i;
         }
-
-        System.out.println(sum);
-
+        return sum;
     }
 }
--- src/main/java/fundamentals/SecondOptTask.java
+++ src/main/java/fundamentals/SecondOptTask.java
@@ -1,66 +1,81 @@
 package fundamentals;
 
-import sun.font.FontRunIterator;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 
 public class SecondOptTask {
-    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    private static ArrayList<Integer> integerArrayList = new ArrayList<Integer>();
 
-    public static void arrayFiller() throws IOException {
-        System.out.println("Введите количество чисел");
-        int quantity = Integer.parseInt(reader.readLine());
-        System.out.println("Введите числа");
-        for (int i = 0; i < quantity; i++) {
-            try {
-                Integer inpNum = Integer.parseInt(reader.readLine());
-                integerArrayList.add(inpNum);
-            } catch (NumberFormatException numExp) {
-                numExp.printStackTrace();
+    public static void main(String[] args) throws IOException {
+
+        lengthSort(arrayFiller());
+    }
+
+    static ArrayList<Integer> arrayFiller() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            ArrayList<Integer> integerArrayList = new ArrayList<Integer>();
+            System.out.println("Введите количество чисел");
+            int quantity = Integer.parseInt(reader.readLine());
+            System.out.println("Введите числа");
+            for (int i = 0; i < quantity; i++) {
+                try {
+                    Integer inpNum = Integer.parseInt(reader.readLine());
+                    integerArrayList.add(inpNum);
+                } catch (NumberFormatException numExp) {
+                    numExp.printStackTrace();
+                }
             }
+            return integerArrayList;
         }
     }
 
     //Найти самое короткое и самое длинное число и их длину.
-    public static void lengthCounter() {
-        int minNum = integerArrayList.get(0);
-        int maxNum = integerArrayList.get(0);
-        int minLength = String.valueOf(minNum).length();
-        int maxLength = String.valueOf(maxNum).length();
-        int tempMinLength;
-        int tempMaxLength;
-        for (Integer i : integerArrayList) {
-            tempMinLength = String.valueOf(i).length();
-            tempMaxLength = String.valueOf(i).length();
-            if (tempMinLength < minLength) {
-                minNum = i;
-                minLength = tempMinLength;
+    static void lengthCounter(ArrayList<Integer> arrayList) {
+        if (CollectionUtils.isNotEmpty(arrayList)) {
+            int minNum = arrayList.get(0);
+            int maxNum = arrayList.get(0);
+            int minLength = String.valueOf(minNum).length();
+            int maxLength = String.valueOf(maxNum).length();
+            int tempMinLength;
+            int tempMaxLength;
+            for (Integer i : arrayList) {
+                tempMinLength = String.valueOf(i).length();
+                tempMaxLength = String.valueOf(i).length();
+                if (tempMinLength < minLength) {
+                    minNum = i;
+                    minLength = tempMinLength;
+                }
+                if (tempMaxLength > maxLength) {
+                    maxLength = tempMaxLength;
+                    maxNum = i;
+                }
             }
-            if (tempMaxLength > maxLength) {
-                maxLength = tempMaxLength;
-                maxNum = i;
-            }
-        }
 
-        //Вывести найденные числа и их длину.
-        System.out.println("Самое короткое число: " + minNum);
-        System.out.println("Длина самого короткого числа: " + minLength);
-        System.out.println("Самое длинное число: " + maxNum);
-        System.out.println("Длина самого длинного числа: " + maxLength);
+            //Вывести найденные числа и их длину.
+            System.out.println("Самое короткое число: " + minNum);
+            System.out.println("Длина самого короткого числа: " + minLength);
+            System.out.println("Самое длинное число: " + maxNum);
+            System.out.println("Длина самого длинного числа: " + maxLength);
+        } else {
+            System.out.println("Список пуст");
+        }
     }
 
-
     //Вывести числа в порядке возрастания значения их длины
-    public static void lengthSort() {
+    public static void lengthSort(ArrayList<Integer> arrayList) {
 
-        Integer[] arraySorted = new Integer[integerArrayList.size()];
-        arraySorted = integerArrayList.toArray(arraySorted);
+        Integer[] arraySorted = new Integer[arrayList.size()];
+        arraySorted = arrayList.toArray(arraySorted);
 
+        Collections.sort(arrayList, Comparator.comparing(num -> String.valueOf(num).length()));
+        /*
         for (int i = arraySorted.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (String.valueOf(arraySorted[j]).length() > String.valueOf(arraySorted[j + 1]).length()) {
@@ -70,9 +85,12 @@ public class SecondOptTask {
                 }
             }
         }
+        */
 
         System.out.println("Числа в порядке возрастания значения их длины: ");
-        System.out.println(Arrays.toString(arraySorted));
+        for (Integer number : arrayList) {
+            System.out.println(number);
+        }
 
     }
 }
