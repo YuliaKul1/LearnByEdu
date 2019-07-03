aa189b83d3a276dd5fc570975b19ca64bf7201d0
Yulia Kulyasova <ulechek.kul@gmail.com>
2019-07-03 17:30:56 +0400

    add FourthOptTask

--- src/main/java/fundamentals/FourthOptTask.java
+++ src/main/java/fundamentals/FourthOptTask.java
@@ -5,41 +5,66 @@ import sun.font.FontRunIterator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
 
 public class FourthOptTask {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
+            System.out.println("Введите размер матрицы");
             int number = Integer.parseInt(reader.readLine());
-            int[][] intsArr = intCreator(number);
+            System.out.println("Введите наибольшее возможное значение");
+            int bound = Integer.parseInt(reader.readLine());
+            int[][] intsArr = intCreator(number, bound);
+            System.out.println("Изначальная матрица");
             intPrinter(intsArr);
+            System.out.println("Отсортированная матрица");
+            intPrinter(arrSort(intsArr, 3));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    static int[][] intCreator(int n) {
+    static int[][] intCreator(int n, int bound) {
+        Random r = new Random();
         int[][] multiArr = new int[n][n];
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1; j++) {
-                multiArr[i][j] = (int) (Math.random() * 10);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                multiArr[i][j] = r.nextInt(bound * 2) - bound;
             }
-
         }
         return multiArr;
     }
 
-
     static void intPrinter(int[][] multiArr) {
-
-        for (int i = 0; i < multiArr.length; i++) {
-            for (int j = 0; j < multiArr.length; j++) {
-                System.out.print(multiArr[i][j] + "\t");
+        for (int[] number : multiArr) {
+            for (int i = 0; i < number.length; i++) {
+                System.out.print(number[i] + "\t");
             }
             System.out.println();
         }
-
     }
 
+    //упорядочить строки матрицы в порядке возрастания значений kго столбца элементов
+    static int[][] arrSort(int[][] multiArr, int k) {
+
+        //решение через циклы
+        /*  for (int i = 0; i < multiArr.length - 1; i++) {
+            for (int j = 0; j < multiArr.length; j++) {
+                int[] arrTmp = multiArr[i];
+                if (multiArr[i][k] > multiArr[i + 1][k]) {
+                    multiArr[i] = multiArr[i + 1];
+                    multiArr[i + 1] = arrTmp;
+                }
+            }
+        }
+        */
+
+        //решение Java8
+        Arrays.sort(multiArr, Comparator.comparing(a -> a[k]));
+        return multiArr;
+    }
 }
